c873c78a5387c5a591d1a46953eb565fa8986bd3
John Hunter Skolnick <jhskolnick100@gmail.com>
2023-11-14 17:55:02 -0500

    hero color trans

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import Logo from "../assets/logo.png"
 
 
 export default function Hero() {
@@ -7,6 +8,7 @@ export default function Hero() {
     <div className="hero">
       <h1 className="home-intro">Welcome to Virginia Beach</h1>
       <div className="h2-container">
+        <img className="hero-logo"src={Logo}/>
         <h2>we love our people</h2>
         <h2>Come enjoy our community!</h2>
       </div>
