eb760910d4b2756b34d42b573cbc1884cda193ed
John Hunter Skolnick <jhskolnick100@gmail.com>
2023-11-14 19:13:14 -0500

    able to go to companies website

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-
-export default function About(){
-
-    return(
-        <div className="about">
+export default function About() {
+    return (
+      <div className="about">
+        <div className="about-content">
+          <div className="about-text">
             <h1>About Virginia Beach</h1>
             <p>
-Virginia Beach is a vibrant coastal city located in southeastern Virginia, renowned for its beautiful beaches, rich history, and diverse attractions. Stretching along the Atlantic Ocean, it boasts over 35 miles of sandy shores, making it a popular destination for beachgoers, surfers, and water sports enthusiasts.
+            Virginia Beach is a vibrant coastal city located in southeastern Virginia, renowned for its beautiful beaches, rich history, and diverse attractions. Stretching along the Atlantic Ocean, it boasts over 35 miles of sandy shores, making it a popular destination for beachgoers, surfers, and water sports enthusiasts.
 
-The city is a blend of bustling tourist areas and quieter residential neighborhoods. The Oceanfront is the heart of Virginia Beach, featuring the iconic three-mile-long boardwalk lined with hotels, restaurants, shops, and entertainment venues. The boardwalk is perfect for strolling, biking, or simply enjoying the ocean views.
+ The city is a blend of bustling tourist areas and quieter residential neighborhoods. The Oceanfront is the heart of Virginia Beach, featuring the iconic three-mile-long boardwalk lined with hotels, restaurants, shops, and entertainment venues. The boardwalk is perfect for strolling, biking, or simply enjoying the ocean views.
 
 Beyond the beaches, Virginia Beach offers a range of activities and attractions. First Landing State Park provides opportunities for hiking, camping, and exploring nature trails. The Virginia Aquarium & Marine Science Center is a must-visit, showcasing marine life and offering interactive exhibits. Additionally, the Military Aviation Museum and Cape Henry Lighthouse provide insights into the area's history.
 
-The city hosts various events and festivals throughout the year, from music concerts to sporting events, catering to diverse interests. Its dining scene features fresh seafood and a mix of international cuisines, and there are plenty of opportunities for outdoor recreation, including kayaking, fishing, and dolphin-watching tours.
+ The city hosts various events and festivals throughout the year, from music concerts to sporting events, catering to diverse interests. Its dining scene features fresh seafood and a mix of international cuisines, and there are plenty of opportunities for outdoor recreation, including kayaking, fishing, and dolphin-watching tours.
 
-Overall, Virginia Beach combines the relaxation of a beach town with the excitement of a dynamic city, making it a fantastic destination for visitors seeking both leisure and adventure.</p>
-
+            </p>
+          </div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg7WXjO7LtLr7PEphGhUzmoUMlqRgvNkp1vA&usqp=CAU"
+            alt="Virginia Beach"
+            className="about-image"
+          />
         </div>
-    )
-}
+      </div>
+    );
+  }
+  
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,14 +5,12 @@ export default function Card(props){
 
     return(
         <div className="card-container">
-            <a href={props.link}>
-            <div>
-                <img className="card-image" src={props.image}/>
-            </div>
-            <div> 
+            <a href={props.place.link}>
+        
+            <div className="card-items"> 
                 <img className="card-image" src={props.place.image} alt=" image"/>
-                <h2>{props.place.name}</h2>
-                <p>{props.place.description}</p>
+                <h2 className="card-items-text">{props.place.name}</h2>
+                <p className="card-items-text">{props.place.description}</p>
             </div>
 </a>
         </div>
