99b2fa1636c68c920aea19f108aedcb038b9e57a
John Hunter Skolnick <jhskolnick100@gmail.com>
2023-11-15 19:10:35 -0500

    added stuff

--- src/Components/NewAttractions.jsx
+++ src/Components/NewAttractions.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "../Styles/NewAttractions.css"
 import Wavepool from "./Wavepool";
+import Vibe from "./Vibe";
 
 export default function NewAttractions(){
 
     return(
-        <div>
+        <div >
             <h1 className="text-center">New Attractions</h1>
             <Wavepool/>
+            <Vibe />
         </div>
     )
 }
